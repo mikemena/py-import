bc5cf0df999d45769c1484985b54322089f0207b
cooper <tokyolabs@protonmail.com>
2023-03-11 11:10:58 -0500

    wip

---
+++ scripts/example1.py
@@ -0,0 +1,6 @@
+# example1.py
+MY_EX1_STRING = "This is example1 module!"
+
+
+def yolo(x: int):
+    print("You only live", x, "times.")
---
+++ scripts/example2.py
@@ -0,0 +1,11 @@
+# example2.py
+import example1 as el
+import sys
+
+# imported string
+print("The imported string is: ", el.MY_EX1_STRING)
+# imported function
+el.yolo(10)
+
+
+print(sys.path)
