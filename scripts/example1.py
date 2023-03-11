a39db8ae0731936244448fdd30ec16f084960951
cooper <tokyolabs@protonmail.com>
2023-03-11 15:55:13 -0500

    wip

--- scripts/example1.py
+++ scripts/example1.py
@@ -4,3 +4,7 @@ MY_EX1_STRING = "This is example1 module!"
 
 def yolo(x: int):
     print("You only live", x, "times.")
+
+
+if __name__ == "__main__":
+    yolo(10000)
--- scripts/example2.py
+++ scripts/example2.py
@@ -1,11 +1,12 @@
 # example2.py
-import example1 as el
+# import example1 as el
+from example1 import yolo
 import sys
 
 # imported string
-print("The imported string is: ", el.MY_EX1_STRING)
+# print("The imported string is: ", el.MY_EX1_STRING)
 # imported function
-el.yolo(10)
+yolo(10)
 
 
 print(sys.path)
